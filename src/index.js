1839ab82e7f89f588130330e328d0c4ec8bffebe
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-03 07:19:07 +0600

    morgan dev and production setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('./logger');
+const morgan = require('morgan');
+const uuid = require('uuid');
+
+const useMorgan = require('./useMorgan');
+
+const app = express();
+
+useMorgan(app);
+
+app.get('/', (req, res) => {
+  // logger.log(`${req.method} - ${req.url} - ${new Date().toISOString()}`);
+  res.status(200).json({ message: 'welcome' });
+});
+
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000');
+});
---
+++ src/useMorgan.js
@@ -0,0 +1,53 @@
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+morgan.token('random', (req, res) => {
+  return Math.round(Math.random() * 100);
+});
+
+morgan.token('request-id', (req, res) => {
+  return req.isPaused;
+});
+
+const accessLogStream = fs.createWriteStream(
+  path.resolve('logs', 'access.log'),
+  {
+    flags: 'a',
+  }
+);
+
+const useMorgan = (app) => {
+  if (process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'production') {
+    app.use(morgan(prodFormate, { stream: accessLogStream }));
+  } else {
+    app.use(
+      morgan('dev', {
+        skip: function (req, res) {
+          return res.statusCode < 400;
+        },
+        stream: process.stderr,
+      })
+    );
+
+    app.use(
+      morgan('dev', {
+        skip: function (req, res) {
+          return res.statusCode >= 400;
+        },
+        stream: process.stdout,
+      })
+    );
+  }
+};
+
+const prodFormate = (tokens, req, res) => {
+  return JSON.stringify({
+    method: tokens['method'](req, res),
+    status: tokens['status'](req, res),
+    random: tokens['random'](req, res),
+    requestId: tokens['request-id'](req, res),
+  });
+};
+
+module.exports = useMorgan;
